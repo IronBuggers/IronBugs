8b9904ad001b25739392faae024dc17e9ea8c2e0
Tolga Kara <tolgakara2.0@gmail.com>
2020-09-12 11:17:01 +0200

    Local Signup and Login implemented

--- routes/bugArea.js
+++ routes/bugArea.js
@@ -3,7 +3,11 @@ const router = express.Router()
 
 /* GET home page */
 router.get("/", (req, res, next) => {
-	res.render("index")
+	res.render("bug-area")
+})
+
+router.get("/firstSignin", (req, res, next) => {
+	res.render("firstSignin")
 })
 
 module.exports = router
