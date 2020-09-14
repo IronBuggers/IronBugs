4471540bda013f13a275572abd19494f84a7d7ce
Tolga Kara <tolgakara2.0@gmail.com>
2020-09-14 17:48:39 +0200

    Added First Signin Form Functionality

--- routes/auth.js
+++ routes/auth.js
@@ -1,103 +1,93 @@
 const express = require("express")
 const router = express.Router()
-const User = require('../models/User');
-const bcrypt = require('bcrypt')
-const passport = require('passport');
+const User = require("../models/User")
+const bcrypt = require("bcrypt")
+const passport = require("passport")
 
 /* GET home page */
 router.get("/signup", (req, res, next) => {
 	res.render("signup")
 })
 
-router.get('/github', passport.authenticate('github'));
+router.get("/github", passport.authenticate("github"))
 
 router.get(
-  '/auth/github/callback',
-  passport.authenticate('github', {
-    successRedirect: '/bug-area',
-    failureRedirect: '/'
-  })
+	"/auth/github/callback",
+	passport.authenticate("github", {
+		successRedirect: "/bugArea",
+		failureRedirect: "/",
+	})
 )
 
-router.get('/google',
-  passport.authenticate('google', { scope: ['profile', 'email'] }));
+router.get("/google", passport.authenticate("google", { scope: ["profile", "email"] }))
 
-router.get('/google/callback',
-  passport.authenticate('google', { failureRedirect: '/'}),
-  function(req, res) {
-    //console.log(req);
-    res.redirect('/bug-area')
+router.get("/google/callback", passport.authenticate("google", { failureRedirect: "/" }), function (
+	req,
+	res
+) {
+	//console.log(req);
+	res.redirect("/bugArea")
+})
 
+router.post("/signup", (req, res, next) => {
+	const { email, password } = req.body
+	console.log(email)
+	if (password.length < 8) {
+		res.render("signup", { message: "Your password needs to be 8 chars min" })
+		return
+	}
+	if (email === "") {
+		res.render("signup", { message: "Your email cannot be empty" })
+		return
+	}
+	User.findOne({ email: email }).then((found) => {
+		if (found !== null) {
+			res.render("signup", { message: "This email is already taken" })
+		} else {
+			const salt = bcrypt.genSaltSync()
+			const hash = bcrypt.hashSync(password, salt)
 
+			User.create({
+				email: email,
+				password: hash,
+			}).then((dbUser) => {
+				res.redirect("/")
+			})
+		}
+	})
+})
 
-  });
+router.post("/login", (req, res, next) => {
+	console.log("Wir sind im Pfad Login")
+	const { email, password } = req.body
+	console.log(req.body)
+	User.findOne({ email: email })
+		.then((found) => {
+			if (found === null) {
+				res.render("index", { message: "Invalid credentials" })
+				return
+			}
 
-router.post('/signup', (req, res, next) => {
-  const { email, password } = req.body;
-  console.log(email);
-  if (password.length < 8) {
-    res.render('signup', { message: 'Your password needs to be 8 chars min' });
-    return;
-  }
-  if (email === '') {
-    res.render('signup', { message: 'Your email cannot be empty' });
-    return;
-  }
-  User.findOne({ email: email })
-    .then(found => {
-      if (found !== null) {
-        res.render('signup', { message: 'This email is already taken' });
-      } else {
-        const salt = bcrypt.genSaltSync();
-        const hash = bcrypt.hashSync(password, salt);
+			if (bcrypt.compareSync(password, found.password)) {
+				req.session.user = found
+				res.redirect("/bugArea")
+			} else {
+				res.render("index", { message: "Invalid credentials" })
+			}
+		})
+		.catch((error) => {
+			next(error)
+		})
+})
 
-        User.create({
-          email: email,
-          password: hash
-        })
-          .then(dbUser => {
-            res.redirect('/');
-          })
-      }
-    })
-
-});
-
-router.post('/login', (req, res, next) => {
-  console.log('Wir sind im Pfad Login');
-  const { email, password } = req.body;
-  console.log(req.body);
-  User.findOne({ email: email })
-    .then(found => {
-
-      if (found === null) {
-        res.render('index', { message: 'Invalid credentials' });
-        return;
-      }
-
-      if (bcrypt.compareSync(password, found.password)) {
-
-        req.session.user = found;
-          res.redirect('/bug-area');
-
-
-      } else {
-        res.render('index', { message: 'Invalid credentials' });
-      }
-    })
-    .catch(error => {
-      next(error);
-    })
-});
-
-router.get('/logout', (req, res) => {
-  req.session.destroy(error => {
-    if (error) {
-      next(error);
-    } else {
-      res.redirect('/');
-    }
-  })
+router.get("/logout", (req, res) => {
+	req.session.destroy((error) => {
+		if (error) {
+			next(error)
+		} else {
+			res.redirect("/")
+		}
+	})
 })
 
 module.exports = router
