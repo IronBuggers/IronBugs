f1d4e47ac5deb6dcec9c9aea272c7c12dbd94f7c
Hamlet <hayrapetyan.hamlet@gmail.com>
2020-09-17 16:37:14 +0200

    github login

--- app.js
+++ app.js
@@ -93,7 +93,7 @@ passport.use(
 		{
 			clientID: process.env.GITHUB_ID,
 			clientSecret: process.env.GITHUB_SECRET,
-			callbackURL: "http://127.0.0.1:3000/auth/github/callback",
+			callbackURL: "https://ironbugs.herokuapp.com/auth/github/callback",
 		},
 		(accessToken, refreshToken, profile, done) => {
 			// find a user with profile.id as githubId or create one
