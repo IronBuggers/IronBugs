acb36dfcdb4d4eaee62c9e2c0f95cbfd6c5e9159
Tolga Kara <tolgakara2.0@gmail.com>
2020-09-17 13:52:42 +0200

    Added Filter, Cleaned and Catching Errors

--- models/Bug.js
+++ models/Bug.js
@@ -1,47 +1,49 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const bugSchema = new Schema({
-  title: String,
-  description: String,
-  imgName: {
-    type: String,
-    default: "",
-  },
-  imgPath: {
-    type: String,
-    default: "",
-  },
-  imgPublicId: {
-    type: String,
-    default: "",
-  },
-  userId: {
-    type: Schema.Types.ObjectId,
-    ref: 'User'
-  },
-  anonym: {
-    type: Boolean,
-    default: false,
-  },
-  
-  bugnumber: Number,
-  
-  status: {
-    type: String,
-    default: 'Active',
-  },
-  votes: [{
-    type: Schema.Types.ObjectId,
-    ref: 'User',
-  }]
+const bugSchema = new Schema(
+	{
+		location: String,
+		title: String,
+		description: String,
+		imgName: {
+			type: String,
+			default: "",
+		},
+		imgPath: {
+			type: String,
+			default: "",
+		},
+		imgPublicId: {
+			type: String,
+			default: "",
+		},
+		userId: {
+			type: Schema.Types.ObjectId,
+			ref: "User",
+		},
+		anonym: {
+			type: Boolean,
+			default: false,
+		},
 
-},
-{
-  timestamps: true
-});
+		bugnumber: Number,
 
+		status: {
+			type: String,
+			default: "Active",
+		},
+		votes: [
+			{
+				type: Schema.Types.ObjectId,
+				ref: "User",
+			},
+		],
+	},
+	{
+		timestamps: true,
+	}
+);
 
-
-const Bug = mongoose.model('Bug', bugSchema);
+const Bug = mongoose.model("Bug", bugSchema);
 
 module.exports = Bug;
